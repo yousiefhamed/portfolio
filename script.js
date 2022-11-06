2d7353b4d19df22e49f6637773bf17794a281e06
youssef hamed <107797980+yousiefhamed@users.noreply.github.com>
2022-11-06 22:56:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById('about').addEventListener('click', () => {
+  document.getElementById('aboutSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('aboutSec').addEventListener('click', () => {
+  document.getElementById('aboutSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('services').addEventListener('click', () => {
+  document.getElementById('servicesSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('servicesSec').addEventListener('click', () => {
+  document.getElementById('servicesSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('projects').addEventListener('click', () => {
+  document.getElementById('projectsSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('projectsSec').addEventListener('click', () => {
+  document.getElementById('projectsSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('contact').addEventListener('click', () => {
+  document.getElementById('contactSec').scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById('contactSec').addEventListener('click', () => {
+  document.getElementById('contactSec').scrollIntoView({ behavior: "smooth" });
+});
